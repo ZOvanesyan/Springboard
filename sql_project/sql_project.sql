f408b92cf39ce57a6d4485e5dc5fdedbbee1d23c
zaven <you@example.com>
2019-01-07 21:28:32 -0800

    Removed output

--- sql_project/sql_project.sql
+++ sql_project/sql_project.sql
@@ -31,35 +31,35 @@ SELECT name
 FROM Facilities
 WHERE membercost >0
 
-name
-Tennis Court 1
-Tennis Court 2
-Massage Room 1
-Massage Room 2
-Squash Court
 
 /* Q2: How many facilities do not charge a fee to members? */
 
 SELECT COUNT(*) FROM Facilities
 WHERE membercost = 0
 
-COUNT(*)
-4
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM  `Facilities` 
+WHERE membercost < 0.20 * monthlymaintenance 
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
 
+SELECT * 
+FROM  `Facilities` 
+WHERE facid
+IN ( 1, 5 ) 
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
+
 
 
 /* Q6: You'd like to get the first and last name of the last member(s)
